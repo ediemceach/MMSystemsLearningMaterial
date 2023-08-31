4c7f53f2ce01f36debc64a5d047b98dad596106d
ediemcaech <emekic@np.ac.rs>
2023-08-31 21:22:28 +0200

    Brisanje svih promenljivih iz klase i uvođenje nove promenljive, author_second_name, ne rezultira u grešci, budući da PHP omogućava dinamičko deklarisanje promenljivih.

--- newfile.php
+++ newfile.php
@@ -1,9 +1,6 @@
 <?php
 class ShopProduct {
-    public $title="Default";
-        public $author_main_name="surname";
-        public $author_first_name="name";
-        public $price=0;
+
 }
 
 $book1= new ShopProduct();
@@ -15,7 +12,7 @@ $book1->price = 19.99;
 $book2= new ShopProduct();
 $book2->title="Book two";
 $book2->author_main_name = "Smith";
-$book2->author_first_name = "Jane";
+$book2->author_second_name = "Jane";
 $book2->price = 29.99;
 
 var_dump($book1);
