0bf87e1904b5ee1aae8b4e7856001b0a28d8ed5d
ediemcaech <emekic@np.ac.rs>
2023-08-31 10:52:14 +0200

    Create two initial instances named book1 and book2 using the provided code, then utilize the var_dump function to display their respective values. The code involves the definition of the ShopProduct class.

--- newfile.php
+++ newfile.php
@@ -5,4 +5,19 @@ class ShopProduct {
         public $author_first_name="name";
         public $price=0;
 }
+
+$book1= new ShopProduct();
+$book1->title = "Book One";
+$book1->author_main_name = "Doe";
+$book1->author_first_name = "John";
+$book1->price = 19.99;
+
+$book2= new ShopProduct();
+$book2->title="Book two";
+$book2->author_main_name = "Smith";
+$book2->author_first_name = "Jane";
+$book2->price = 29.99;
+
+var_dump($book1);
+var_dump($book2);
 ?>
