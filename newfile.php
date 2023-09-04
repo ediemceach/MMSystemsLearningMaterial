a95335b608c76fb22181f566ab1f8455d79c6b28
ediemcaech <emekic@np.ac.rs>
2023-09-04 22:54:41 +0200

    We establish two methods for obtaining organized data, proceed to generate two instances, $book1 and $book2, allocate specific values to their attributes, and subsequently display both the author's complete name and the product's title and price by employing the defined methods.

--- newfile.php
+++ newfile.php
@@ -1,20 +1,31 @@
 <?php
 class ShopProduct {
-
+    public $title="title";
+    public $author_sur_name="surname";
+    public $author_first_name="name";
+    public $price=0;
+    
+    public function NameSurname(){
+        return $this->author_first_name . " " . $this->author_sur_name;
+    }
+    
+    public function TitlePrice(){
+        return  $this->title . " price " .  $this->price;
+    }
 }
 
 $book1= new ShopProduct();
 $book1->title = "Book One";
-$book1->author_main_name = "Doe";
+$book1->author_sur_name = "Doe";
 $book1->author_first_name = "John";
 $book1->price = 19.99;
 
 $book2= new ShopProduct();
 $book2->title="Book two";
-$book2->author_main_name = "Smith";
-$book2->author_second_name = "Jane";
+$book2->author_sur_name = "Smith";
+$book2->author_first_name = "Jane";
 $book2->price = 29.99;
 
-var_dump($book1);
-var_dump($book2);
+print "author: {$book1->NameSurname()}";
+print "Title: {$book1->TitlePrice()}";
 ?>
