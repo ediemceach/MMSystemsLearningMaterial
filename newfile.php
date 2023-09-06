59e8c023e800d89311c95fa02d2903da6968cd41
ediemcaech <emekic@np.ac.rs>
2023-09-06 18:37:26 +0200

    Pod oblast argumenti i tipovi

--- newfile.php
+++ newfile.php
@@ -1,32 +1,5 @@
 <?php
 class ShopProduct {
 
-    
-    public function __construct(
-       public $title,
-        public $author_sur_name="",
-        public $author_first_name="",
-        public $price=0)
-    {
 
-    }
-    
-    public function NameSurname(){
-        return $this->author_first_name . " " . $this->author_sur_name;
-    }
-    
-    public function TitlePrice(){
-        return  $this->title . " price " .  $this->price;
-    }
-}
-
-$book1= new ShopProduct("Book One");
-$book2= new ShopProduct(price:10, title:"Proba");
-
-
-
-print "author: {$book1->NameSurname()}";
-print "Title: {$book1->TitlePrice()}";
-print "author: {$book2->NameSurname()}";
-print "Title: {$book2->TitlePrice()}";
 ?>
