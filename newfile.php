37327190249ad5d978466a1bdc011208be405056
ediemcaech <emekic@np.ac.rs>
2023-09-09 09:20:31 +0200

    Nullable tipovi

--- newfile.php
+++ newfile.php
@@ -1,28 +1,19 @@
 <?php
 class Storage{
-    public function add(string $key, $value){
+    public function add(string $key,?string $value){
         //ovakva funkcija radi nesto sa promenljivim key i value koji moraju biti stringovi
     }
     
-    public function add1(string $kew, mixed $valuse){
-        //ova funkcija ogranicava key na string, dok je value tipa mixed(tj. moze biti array, bool, callable, int, float, null, object, resource, or string
-        
+    public fuction getValue():int{
+        return $this->Value;
     }
     
-    public function Union(string $key, string|bool $value){
-        
-        //value je na ovakav nacin ogranicen na string ili boolean
-        
+    public function Discount():int|float{
+        return ($this->price-$this-discount);
     }
     
-    public function Union(string $key, string|bool|null $value){
-        
-        //value je na ovakav nacin ogranicen na string ili boolean, ali i na null vrednost
-        
-    }
-    
-    public setShopProduct(ShopProduct|null $product){
-        
+    public function setDiscount(int|float $num):void{
+        $this->discoun=$num;
     }
 }
 
