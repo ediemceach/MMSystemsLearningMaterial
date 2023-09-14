21b2755695c69ce526e7032a7f413b56651f87c7
ediemcaech <emekic@np.ac.rs>
2023-09-14 18:41:21 +0200

    Advanced OOP properties

--- newfile.php
+++ newfile.php
@@ -1,1 +1,19 @@
 <?php
+
+class ExampeStatic
+
+{
+    
+    public static int $aNum=0;
+    public static function sayHello(): void
+    {
+        print "hello";
+    }
+    
+}
+
+
+print ExampeStatic::$aNum;
+ExampeStatic::sayhello();
+
+?>
