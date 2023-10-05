47f03f5884b0a5c6f8564df332c5a008531b7fdf
ediemcaech <emekic@np.ac.rs>
2023-10-05 17:26:01 +0200

    Definisan Interfejs za uzimanje podataka objekta, Objekat ShopProduct i dva dete objekta cdProdukt i bookProduct

--- newfile.php
+++ newfile.php
@@ -1,6 +1,27 @@
 <?php
 
-class ShopProduct
+interface classDataGetter{
+    
+    
+    
+    public function getFirstName(): ?string;
+    
+    public function getSurname(): ?string;
+  
+    public function getTitle(): string;
+    
+    public function getPrice():int|float;
+    
+    public function getDiscount(): int|float;
+    
+    public function getplayLenght(): int|float;
+    
+
+    
+}
+
+
+class ShopProduct implements classDataGetter
 {
     public function __construct(
         private string $title,
@@ -20,16 +41,12 @@ class ShopProduct
     public function getSurname(): ?string
     {
         return $this->authorSurName;
+        
     }
     
-    public function setDiscount(int|float $num)
+    public function getPrice(): int|float
     {
-        $this->discount = $num;
-    }
-    
-    public function getDiscount(): int|float
-    {
-        return $this->discount;
+        return $this->price;
     }
     
     public function getTitle(): string
@@ -37,226 +54,57 @@ class ShopProduct
         return $this->title;
     }
     
-    public function getPrice(): int|float
+    public function getDiscount(): int|float
     {
-        return $this->price;
-    }
-}
-
-class CdProduct extends ShopProduct
-{
-    public int $playLength = 0;
-    
-    public function __construct(
-        string $title,
-        ?string $authorSurName = null,
-        ?string $authorFirstName = null,
-        float $price = 0,
-        int $playLength = 0
-        ) {
-            parent::__construct($title, $authorSurName, $authorFirstName, $price);
-            $this->playLength = $playLength;
+        return $this->discount;
     }
     
-    public function getPlayLength(): int
+    public function getplayLenght(): int|float
     {
-        return $this->playLength;
-    }
-}
-
-class BookProduct extends ShopProduct
-{
-    public int $numPages = 0;
-    
-    public function __construct(
-        string $title,
-        ?string $authorSurName = null,
-        ?string $authorFirstName = null,
-        float $price = 0,
-        int $numPages = 0
-        ) {
-            parent::__construct($title, $authorSurName, $authorFirstName, $price);
-            $this->numPages = $numPages;
+        return $this->playLenght;
     }
     
-    public function getNumberPages(): int
+    public function numPages(): int|float
     {
         return $this->numPages;
     }
+    
 }
-
-abstract class ShopProductWriter
-{
-    protected array $products = [];
-    
-    public function addProduct(ShopProduct $shopProduct): void
+    class CDProduct extends ShopProduct implements classdataGetter 
     {
-        $this->products[] = $shopProduct;
-    }
     
-    abstract public function printName(ShopProduct $product): string;
-    abstract public function printSurname(ShopProduct $product): string;
-    abstract public function printNameSurname(ShopProduct $product): string;
-    abstract public function printPlayLength(ShopProduct $product): int;
-    abstract public function printNumberPages(ShopProduct $product): int;
-    abstract public function printDiscountedPrice(ShopProduct $product): float;
-    abstract public function printSummary(ShopProduct $product): string;
-    abstract public function printXML(): void;
-}
-
-class ConcreteShopProductWriter extends ShopProductWriter
-{
-    public function printName(ShopProduct $product): string
-    {
-        // Implementation
-        return $product->getFirstName();
-    }
-    
-    public function printSurname(ShopProduct $product): string
-    {
-        // Implementation
-        return $product->getSurname();
-    }
-    
-    public function printNameSurname(ShopProduct $product): string
-    {
-        // Implementation
-        return "Author's First Name: " . $product->getFirstName() . "; Author's Last Name: " . $product->getSurname() . " \n";
-    }
-    
-    public function printPlayLength(ShopProduct $product): int
-    {
-        // Implementation
-        return $product->getPlayLength();
-    }
-    
-    public function printNumberPages(ShopProduct $product): int
-    {
-        // Implementation
-        return $product->getNumberPages();
-    }
-    
-    public function printDiscountedPrice(ShopProduct $product): float
-    {
-        // Implementation
-        $discountedPrice = $product->getPrice() - ($product->getPrice() * $product->getDiscount());
-        return $discountedPrice;
-    }
-    
-    public function printSummary(ShopProduct $product): string
-    {
-        // Implementation
-        $base =
-        "Author's First Name: {$product->getFirstName()} Author's Last Name: {$product->getSurname()}\n" .
-        "Title: {$product->getTitle()}" .
-        "Price: {$product->getPrice()}\n";
-        return $base;
-    }
-    
-    public function printXML(): void
-    {
-        $writer = new \XMLWriter();
-        $writer->openMemory();
-        $writer->startDocument('1.0', 'UTF-8');
-        $writer->startElement("products");
+        public int $playLength = 0;
         
-        foreach ($this->products as $shopProduct) {
-            $writer->startElement("product");
-            $writer->writeAttribute("title", $shopProduct->getTitle());
-            $writer->startElement("summary");
-            $writer->text($this->printSummary($shopProduct));
-            $writer->endElement();
-            $writer->endElement();
+        public function __construct(
+            string $title,
+            ?string $authorSurName = null,
+            ?string $authorFirstName = null,
+            float $price = 0,
+            int $playLength = 0
+            ) {
+                parent::__construct($title, $authorSurName, $authorFirstName, $price);
+                $this->playLength = $playLength;
         }
         
-        $writer->endElement();
-        $writer->endDocument();
-        
-        // Save the XML to a file (adjust the file path as needed)
-        $xmlFilePath = 'products.xml';
-        file_put_contents($xmlFilePath, $writer->flush());
-        
-        echo "XML file created successfully at: $xmlFilePath";
     }
-}
 
-// Instantiate the concrete class
-$shopProductWriter = new ConcreteShopProductWriter();
-
-// Fetch all data from the products table
-function fetchAllProductData()
-{
-    try {
-        // Create a PDO database connection
-        $db = new PDO('sqlite:shopproduct.db');
+    class BookProduct extends ShopProduct implements classdataGetter
+    {
         
-        // Define the SQL query to fetch all product data
-        $query = "SELECT * FROM products";
+        public int $numPages = 0;
         
-        // Prepare the SQL statement
-        $statement = $db->prepare($query);
-        
-        // Execute the query
-        $statement->execute();
-        
-        // Fetch all results as an associative array
-        $productDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
-        
-        // Close the database connection
-        $db = null;
-        
-        return $productDataList;
-    } catch (PDOException $e) {
-        // Handle any database connection errors
-        echo "Error: " . $e->getMessage();
-        return false; // Or handle the error in a way that makes sense for your application
-    }
-}
-
-// Fetch all product data
-$productDataList = fetchAllProductData();
-
-// Check if data is fetched successfully
-if ($productDataList) {
-    foreach ($productDataList as $productData) {
-        // Determine the product type
-        $productType = $productData['type'];
-        
-        // Create an instance based on the product type
-        if ($productType === 'book') {
-            $product = new BookProduct(
-                $productData['title'],
-                $productData['surname'],
-                $productData['name'],
-                $productData['price'],
-                $productData['numpages']
-                );
-            // Set additional properties specific to BookProduct
-            $product->setDiscount($productData['discount']);
-        } elseif ($productType === 'cd') {
-            $product = new CdProduct(
-                $productData['title'],
-                $productData['surname'],
-                $productData['name'],
-                $productData['price'],
-                $productData['playlength']
-                );
-            // Set additional properties specific to CdProduct
-            // (if any, for now, CdProduct doesn't have additional properties)
-        } else {
-            // Handle unknown product type
-            echo "Unknown product type: $productType";
-            continue;
+        public function __construct(
+            string $title,
+            ?string $authorSurName = null,
+            ?string $authorFirstName = null,
+            float $price = 0,
+            int $numPages = 0
+            ) {
+                parent::__construct($title, $authorSurName, $authorFirstName, $price);
+                $this->numPages = $numPages;
         }
         
-        // Add the product to the ShopProductWriter's products array
-        $shopProductWriter->addProduct($product);
     }
-    
-    // Print XML for all products
-    $shopProductWriter->printXML();
-} else {
-    echo "No products found in the database.";
-}
+
 ?>
 
