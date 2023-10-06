c30b75f6e080df0078609fd4d61ee86afd2c9a31
ediemcaech <emekic@np.ac.rs>
2023-10-06 10:15:36 +0200

    Interfejsi, implementacija istog koda koriscenjem interfesjsa umesto abstraktnih klasa

--- newfile.php
+++ newfile.php
@@ -1,110 +1,175 @@
 <?php
 
-interface classDataGetter{
-    
-    
-    
+interface ClassDataGetter {
     public function getFirstName(): ?string;
-    
     public function getSurname(): ?string;
-  
     public function getTitle(): string;
-    
-    public function getPrice():int|float;
-    
+    public function getPrice(): int|float;
     public function getDiscount(): int|float;
-    
-    public function getplayLenght(): int|float;
-    
-
-    
+    public function getPlayLength(): int|float;
+    public function getNumPages(): int|float;
+    public function getSummary(): string;
 }
 
-
-class ShopProduct implements classDataGetter
-{
+class ShopProduct implements ClassDataGetter {
+    private int|float $discount;
+    protected int|float $playLength;
+    protected int|float $numPages;
+    protected string $summary;
+    protected ?string $authorSurName;
+    protected ?string $authorFirstName;
+    protected int|float $price;
+    
     public function __construct(
         private string $title,
-        private ?string $authorSurName = null,
-        private ?string $authorFirstName = null,
-        protected int|float $price = 0
+        ?string $authorSurName = null,
+        ?string $authorFirstName = null,
+        int|float $price = 0
         ) {
+            $this->authorSurName = $authorSurName;
+            $this->authorFirstName = $authorFirstName;
+            $this->price = $price;
     }
     
-    private int|float $discount;
-    
-    public function getFirstName(): ?string
-    {
+    public function getFirstName(): ?string {
         return $this->authorFirstName;
     }
     
-    public function getSurname(): ?string
-    {
+    public function getSurname(): ?string {
         return $this->authorSurName;
-        
     }
     
-    public function getPrice(): int|float
-    {
+    public function getPrice(): int|float {
         return $this->price;
     }
     
-    public function getTitle(): string
-    {
+    public function getTitle(): string {
         return $this->title;
     }
     
-    public function getDiscount(): int|float
-    {
+    public function getDiscount(): int|float {
         return $this->discount;
     }
     
-    public function getplayLenght(): int|float
-    {
-        return $this->playLenght;
+    public function getPlayLength(): int|float {
+        return $this->playLength;
     }
     
-    public function numPages(): int|float
-    {
+    public function getNumPages(): int|float {
         return $this->numPages;
     }
     
+    public function getSummary(): string {
+        $base =
+        "AFN: {$this->getFirstName()} Author's Last Name: {$this->getSurname()}\n" .
+        "Title: {$this->getTitle()}\n" .
+        "Price: {$this->getPrice()}\n" .
+        "Number pages: {$this->getNumPages()}\n";
+        return $base;
+    }
 }
-    class CDProduct extends ShopProduct implements classdataGetter 
-    {
-    
-        public int $playLength = 0;
-        
-        public function __construct(
-            string $title,
-            ?string $authorSurName = null,
-            ?string $authorFirstName = null,
-            float $price = 0,
-            int $playLength = 0
-            ) {
-                parent::__construct($title, $authorSurName, $authorFirstName, $price);
-                $this->playLength = $playLength;
-        }
-        
-    }
 
-    class BookProduct extends ShopProduct implements classdataGetter
-    {
-        
-        public int $numPages = 0;
-        
-        public function __construct(
-            string $title,
-            ?string $authorSurName = null,
-            ?string $authorFirstName = null,
-            float $price = 0,
-            int $numPages = 0
-            ) {
-                parent::__construct($title, $authorSurName, $authorFirstName, $price);
-                $this->numPages = $numPages;
-        }
-        
+class CDProduct extends ShopProduct {
+    public function __construct(
+        string $title,
+        ?string $authorSurName = null,
+        ?string $authorFirstName = null,
+        float $price = 0,
+        int $playLength = 0
+        ) {
+            parent::__construct($title, $authorSurName, $authorFirstName, $price);
+            $this->playLength = $playLength;
     }
+    
+    public function getSummary(): string {
+        $base =
+        "Author's First Name: {$this->getFirstName()} Author's Last Name: {$this->getSurname()}\n" .
+        "Title: {$this->getTitle()}\n" .
+        "Price: {$this->getPrice()}\n" .
+        "Length: {$this->getPlayLength()}\n";
+        return $base;
+    }
+}
+
+class BookProduct extends ShopProduct {
+    public function __construct(
+        string $title,
+        ?string $authorSurName = null,
+        ?string $authorFirstName = null,
+        float $price = 0,
+        int $numPages = 0
+        ) {
+            parent::__construct($title, $authorSurName, $authorFirstName, $price);
+            $this->numPages = $numPages;
+    }
+    
+    public function getSummary(): string {
+        $base =
+        "Author's First Name: {$this->getFirstName()} Author's Last Name: {$this->getSurname()}\n" .
+        "Title: {$this->getTitle()}\n" .
+        "Price: {$this->getPrice()}\n" .
+        "Number pages: {$this->getNumPages()}\n";
+        return $base;
+    }
+}
+
+class ShopProductPrinter  {
+    protected array $products = [];
+    
+    public function addProduct(ShopProduct $shopProduct): void
+    {
+        $this->products[] = $shopProduct;
+    }
+    
+    public function printFirstName(ShopProduct $product): string
+    {
+        $base ="Author name: {$product->getFirstName()} \n";
+        return $base;
+    }
+    
+    public function printSurname(ShopProduct $product): ?string
+    {
+        return $product->getSurname();
+    }
+    
+    public function printPrice(ShopProduct $product): int|float
+    {
+        return $product->getPrice();
+    }
+    
+    public function printTitle(ShopProduct $product): string
+    {
+        return $product->getTitle();
+    }
+    
+    public function printDiscount(ShopProduct $product): int|float
+    {
+        return $product->getDiscount();
+    }
+    
+    public function printPlayLength(ShopProduct $product): int|float
+    {
+        return $product->getPlayLength();
+    }
+    
+    public function printNumPages(ShopProduct $product): int|float
+    {
+        return $product->getNumPages();
+    }
+    
+    public function printSummary(ShopProduct $product): string
+    {
+        return $product->getNumPages();
+    }
+}
+
+// Usage example
+$cdProduct = new CDProduct('CD Title', 'Author', 'John Doe', 15.99, 120);
+$bookProduct = new BookProduct('Book Title', 'Author', 'Jane Doe', 19.99, 300);
+
+echo $cdProduct->getSummary();
+
+$writer = new ShopProductPrinter();
+echo $writer->printFirstName($cdProduct);
 
 ?>
-
