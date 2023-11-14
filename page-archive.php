b6c2e99fbcd28dfc7f17782b2cadf07a7d0239b2
ediemcaech <emekic@np.ac.rs>
2023-11-14 23:48:08 +0100

    Dodali smo osnovne funkcionalnosti u templejt Archive, u Wordpessu pravimo novu stranicu, biramo templejt Archive i sadrzaj yavrseen strane dobijamo tu gde smo ga planirali

--- page-archive.php
+++ page-archive.php
@@ -3,3 +3,16 @@
 Template Name: Archives
  */
 ?>
+
+<h2><?php _e('Browse by Month:', 'notesblog'); ?></h2>
+<ul>
+    <?php wp_get_archives('type=monthly'); ?>
+</ul>
+
+<h2><?php _e('Browse by Category:', 'notesblog'); ?></h2>
+<ul>
+    <?php wp_list_categories('title_li='); ?>
+</ul>
+
+<h2><?php _e('Browse by Tag:', 'notesblog'); ?></h2>
+<?php wp_tag_cloud('smallest=8&largest=28&number=0&orderby=name&order=ASC');
