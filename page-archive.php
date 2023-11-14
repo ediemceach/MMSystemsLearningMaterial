bd59cc69969fa49afec3deea544b105559089745
ediemcaech <emekic@np.ac.rs>
2023-11-14 23:51:52 +0100

    Konacno zavrsen templejt za Archives

--- page-archive.php
+++ page-archive.php
@@ -1,18 +1,31 @@
 <?php
 /*
-Template Name: Archives
+ Template Name: Archives
  */
 ?>
+<?php get_header(); ?>
+<div id="content" class="widecolumn">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <h1><?php the_title(); ?></h1>
+            <div class="entry">
+                <?php the_content(); ?>
+                <h2><?php _e('Browse by Month:', 'notesblog'); ?></h2>
+                <ul>
+                    <?php wp_get_archives('type=monthly'); ?>
+                </ul>
+                <h2><?php _e('Browse by Category:', 'notesblog'); ?></h2>
+                <ul>
+                    <?php wp_list_categories('title_li='); ?>
+                </ul>
+                <h2><?php _e('Browse by Tag:', 'notesblog'); ?></h2>
+                <?php wp_tag_cloud('smallest=8&largest=28&number=0&orderby=name&order=ASC'); ?>
+            </div>
+        </div>
+    <?php endwhile; ?>
+    <?php endif; ?>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
 
-<h2><?php _e('Browse by Month:', 'notesblog'); ?></h2>
-<ul>
-    <?php wp_get_archives('type=monthly'); ?>
-</ul>
 
-<h2><?php _e('Browse by Category:', 'notesblog'); ?></h2>
-<ul>
-    <?php wp_list_categories('title_li='); ?>
-</ul>
-
-<h2><?php _e('Browse by Tag:', 'notesblog'); ?></h2>
-<?php wp_tag_cloud('smallest=8&largest=28&number=0&orderby=name&order=ASC');
