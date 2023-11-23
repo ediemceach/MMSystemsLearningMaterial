cd65bb0a54db14b1101b89976fe5e722a42c40c2
ediemcaech <emekic@np.ac.rs>
2023-11-23 19:16:00 +0100

    Formiranje plugin.php fajla sa osnovnim podacima o plaginu. Na slici1 imamo situaciju pre dodavanja koda i na slici2 nakon dodavanja koda

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       MojPlugin
+ * Plugin URI:        https://example.com/plugins/pdev
+ * Description:       Kratki opis PlugIn
+ * Version:           1.0.0
+ * Requires at least: 5.3
+ * Requires PHP:      5.6
+ * Author:            Edis Mekic
+ * Author URI:        https://example.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       pdev
+ * Domain Path:       /public/lang
+ */
+
+?>
