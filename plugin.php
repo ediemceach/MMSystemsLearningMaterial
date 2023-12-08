cc167869494a49af5563315ae2fc748db823c44c
ediemcaech <emekic@np.ac.rs>
2023-12-08 23:47:36 +0100

    Dodavanje buttona iz baze WordPressa

--- plugin.php
+++ plugin.php
@@ -22,22 +22,20 @@ function pdev_styling_settings() {
     ?>
     <div class="wrap">
 
-    <div class="notice notice-error is-dismissible">
-        <p>There has been an error.</p>
-    </div>
- 
-    <div class="notice notice-warning is-dismissible">
-        <p>This is a warning message.</p>
-    </div>
- 
-    <div class="notice notice-success is-dismissible">
-        <p>This is a success message.</p>
-    </div>
- 
-    <div class="notice notice-info is-dismissible">
-        <p>This is some information.</p>
-        
-        
+<p>
+<input type="submit" name="Save" value="Save Options"/>
+<input type="submit" name="Save" value="Save Options" 
+    class="button-primary"/>
+</p><p>
+<input type="submit" name="Secondary" value="Secondary Button"/>
+<input type="submit" name="Secondary" value="Secondary Button" 
+    class="button-secondary"/>
+</p>
+
+
+<a href="#">Search</a>
+<a href="#" class="button-primary">Search Primary</a>
+<a href="#" class="button-secondary">Search Secondary</a>
         
     </div>
     <?php
