388b255df870204429b265886a9dc3aa2c9f5561
ediemcaech <emekic@np.ac.rs>
2023-12-08 23:51:15 +0100

    Dodavanje polja, buttona i pull down menija

--- plugin.php
+++ plugin.php
@@ -22,20 +22,55 @@ function pdev_styling_settings() {
     ?>
     <div class="wrap">
 
-<p>
-<input type="submit" name="Save" value="Save Options"/>
-<input type="submit" name="Save" value="Save Options" 
-    class="button-primary"/>
-</p><p>
-<input type="submit" name="Secondary" value="Secondary Button"/>
-<input type="submit" name="Secondary" value="Secondary Button" 
-    class="button-secondary"/>
-</p>
-
-
-<a href="#">Search</a>
-<a href="#" class="button-primary">Search Primary</a>
-<a href="#" class="button-secondary">Search Secondary</a>
+ <?php screen_icon( 'plugins' ); ?>
+        <h2>My Plugin</h2>
+        <form method="POST" action="">
+        <table class="form-table">
+        <tr valign="top">
+            <th scope="row"><label for="fname">First Name</label></th>
+            <td><input maxlength="45" size="25" name="fname"/></td>
+        </tr>
+        
+        
+        <tr valign="top">
+            <th scope="row"><label for="lname">Last Name</label></th>
+            <td><input id="lname" maxlength="45" size="25" name="lname"/></td>
+        </tr>
+        
+        <tr valign="top">
+            <th scope="row"><label for="color">Favorite Color</label></th>
+            <td>
+                <select name="color">
+                    <option value="orange">Orange</option>
+                    <option value="black">Black</option>
+                </select>
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><label for="featured">Featured?</label></th>
+            <td><input type="checkbox" name="favorite"/></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><label for="gender">Gender</label></th>
+            <td>
+                <input type="radio" name="gender" value="male"/> Male
+                <input type="radio" name="gender" value="female"/> Female
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><label for="bio">Bio</label></th>
+            <td><textarea name="bio"></textarea></td>
+        </tr>
+        <tr valign="top">
+            <td>
+            <input type="submit" name="save" value="Save Options"
+                class="button-primary"/>
+            <input type="submit" name="reset" value="Reset"
+                class="button-secondary"/>
+            </td>
+        </tr>
+        </table>
+        </form>
         
     </div>
     <?php
