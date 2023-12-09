8934213eed0adb7fda5669dfe21bea95590b7598
ediemcaech <emekic@np.ac.rs>
2023-12-09 23:00:56 +0100

    Postavljamo osnovu za formiranje PlugIn-a Nonce Example

---
+++ plugin.php
@@ -0,0 +1,8 @@
+ <?php
+/**
+ * Plugin Name: Nonce Example
+ * Plugin URI:  http://example.com/
+ * Description: Displays an example nonce field and verifies it.
+ * Author:      WROX
+ * Author URI:  http://wrox.com
+ */
