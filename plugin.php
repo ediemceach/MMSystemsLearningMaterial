d8f57c2675e0a12419acf7a701a4da70a062ebb2
ediemcaech <emekic@np.ac.rs>
2023-12-10 13:41:49 +0100

    Formirali smo funkciju function pdev_nonce_example_template(), cijim pozivanjem function pdev_nonce_example_menu(), formira polja za upis i dugme za snimanje unetog imena

--- plugin.php
+++ plugin.php
@@ -21,7 +21,26 @@ function pdev_nonce_example_menu(){
         );
 }
     
-function pdev_nonce_example_template(){
-    ;
+function pdev_nonce_example_template(){?>
+   <div class=wrap> 
+   
+   <h1 class="wp-heading-inline">Nonce Example</h1>
+   
+   <?php $value = get_option( 'pdev_nonce_example' ); ?>
+   
+   <form method="post" action="">
+    <?php wp_nonce_field( 'pdev_nonce_action', 'pdev_nonce_name' ); ?>
+    
+                              <label>
+                                 Enter your name:
+                                 <input type="text" name="pdev_nonce_example"
+                                 value="<?php echo esc_attr( $value ); ?>"/>
+                          </label>
+                 </p>
+                     <?php submit_button( 'Submit', 'primary' ); ?>
+             </form>
+
+   </div>
+   <?php
 }
  
