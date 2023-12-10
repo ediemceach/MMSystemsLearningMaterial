3fcbe928cf46f309a08d523aa1c6021face40d74
ediemcaech <emekic@np.ac.rs>
2023-12-10 13:18:06 +0100

    Dodali smo akciju admin_menu i dodlai funkcinu za formiranje menija function pdev_nonce_example_menu(), ona poziva funkciju  'pdev_nonce_example_template', koju za sada samo formiramo i ostavljamo praznu.

--- plugin.php
+++ plugin.php
@@ -6,3 +6,22 @@
  * Author:      WROX
  * Author URI:  http://wrox.com
  */
+
+add_action('admin_menu','pdev_nonce_example_menu');
+ 
+
+function pdev_nonce_example_menu(){
+    
+    add_menu_page(
+        'nonce example',
+        'nonce example',
+        'manage_options',
+        'pdev-nonce-example',
+        'pdev_nonce_example_template'
+        );
+}
+    
+function pdev_nonce_example_template(){
+    ;
+}
+ 
