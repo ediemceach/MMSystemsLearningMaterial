d7b8d34929480db1972c70b7f255613c85844e08
ediemcaech <emekic@np.ac.rs>
2023-12-11 21:34:52 +0100

    Sledeci kod je sklonio nonce zastitu Remove nonce from code

--- plugin.php
+++ plugin.php
@@ -24,65 +24,66 @@ function sql_sanitizer_settings_page() {
     ?>
     <div class="wrap">
         <h1><?php esc_html_e('SQL Sanitizer', 'sql-sanitizer'); ?></h1>
+        
         <form method="post" action="">
-            <?php wp_nonce_field('update-post-title-form', 'update-post-title-nonce', true, true); ?>
             <label for="post_id"><?php esc_html_e('Post ID:', 'sql-sanitizer'); ?></label>
             <input type="number" name="post_id" id="post_id" required>
             <br>
             <label for="post_title"><?php esc_html_e('Update Title:', 'sql-sanitizer'); ?></label>
             <input type="text" name="post_title" id="post_title" required>
             <br>
-            <input type="submit" class="button-primary" value="<?php esc_attr_e('Update Post Title', 'sql-sanitizer'); ?>">
+            <input type="submit" name="update_post_title" class="button-primary" value="<?php esc_attr_e('Update Post Title', 'sql-sanitizer'); ?>">
         </form>
 
         <form method="post" action="">
-            <?php wp_nonce_field('get-post-details-form', 'get-post-details-nonce', true, true); ?>
             <input type="submit" name="get_post_details" class="button-secondary" value="<?php esc_attr_e('Get Post Details', 'sql-sanitizer'); ?>">
         </form>
 
         <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && check_admin_referer('get-post-details-form', 'get-post-details-nonce')) {
-            // Get and display post details using $wpdb
-            global $wpdb;
-            $table_name = $wpdb->prefix . 'posts';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['get_post_details'])) {
+                // Get and display post details using $wpdb
+                global $wpdb;
+                $table_name = $wpdb->prefix . 'posts';
 
-            $posts = $wpdb->get_results("SELECT ID, post_title, post_date FROM $table_name");
+                $posts = $wpdb->get_results("SELECT ID, post_title, post_date FROM $table_name");
 
-            echo '<div class="notice">';
-            echo '<p>' . esc_html__('Post Details:', 'sql-sanitizer') . '</p>';
-            echo '<ul>';
-            foreach ($posts as $post) {
-                echo '<li>';
-                echo esc_html__('ID:', 'sql-sanitizer') . ' ' . esc_html($post->ID) . ' | ';
-                echo esc_html__('Title:', 'sql-sanitizer') . ' ' . esc_html($post->post_title) . ' | ';
-                echo esc_html__('Date:', 'sql-sanitizer') . ' ' . esc_html($post->post_date);
-                echo '</li>';
+                echo '<div class="notice">';
+                echo '<p>' . esc_html__('Post Details:', 'sql-sanitizer') . '</p>';
+                echo '<ul>';
+                foreach ($posts as $post) {
+                    echo '<li>';
+                    echo esc_html__('ID:', 'sql-sanitizer') . ' ' . esc_html($post->ID) . ' | ';
+                    echo esc_html__('Title:', 'sql-sanitizer') . ' ' . esc_html($post->post_title) . ' | ';
+                    echo esc_html__('Date:', 'sql-sanitizer') . ' ' . esc_html($post->post_date);
+                    echo '</li>';
+                }
+                echo '</ul>';
+                echo '</div>';
             }
-            echo '</ul>';
-            echo '</div>';
-        }
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && check_admin_referer('update-post-title-form', 'update-post-title-nonce')) {
-            // Sanitize and prepare user input
-            $post_id = absint($_POST['post_id']);
-            $post_title = sanitize_text_field($_POST['post_title']);
+            if (isset($_POST['update_post_title'])) {
+                // Sanitize and prepare user input
+                $post_id = absint($_POST['post_id']);
+                $post_title = sanitize_text_field($_POST['post_title']);
 
-            // Update post title using $wpdb
-            global $wpdb;
-            $table_name = $wpdb->prefix . 'posts';
+                // Update post title using $wpdb
+                global $wpdb;
+                $table_name = $wpdb->prefix . 'posts';
 
-            $wpdb->update(
-                $table_name,
-                array('post_title' => $post_title),
-                array('ID' => $post_id),
-                array('%s'),
-                array('%d')
-            );
+                $wpdb->update(
+                    $table_name,
+                    array('post_title' => $post_title),
+                    array('ID' => $post_id),
+                    array('%s'),
+                    array('%d')
+                );
 
-            // Display success message
-            echo '<div class="notice">';
-            echo '<p>' . esc_html__('Post title updated successfully!', 'sql-sanitizer') . '</p>';
-            echo '</div>';
+                // Display success message
+                echo '<div class="notice">';
+                echo '<p>' . esc_html__('Post title updated successfully!', 'sql-sanitizer') . '</p>';
+                echo '</div>';
+            }
         }
         ?>
     </div>
