2d538e7f2bba3da564c866c32eb581bb98dba0b0
ediemcaech <emekic@np.ac.rs>
2023-12-11 09:17:48 +0100

    Dobijeno iducim promptom: Modify following plugin to start from settings menu:

--- plugin.php
+++ plugin.php
@@ -13,27 +13,50 @@ function secure_query_example_enqueue_styles() {
     wp_enqueue_style('secure-query-example-styles', plugin_dir_url(__FILE__) . 'styles.css');
 }
 
+// Add menu entry under the Settings menu
+add_action('admin_menu', 'secure_query_example_menu');
+function secure_query_example_menu() {
+    add_options_page(
+        'Secure Query Example',
+        'Secure Query Example',
+        'manage_options',
+        'secure-query-example',
+        'secure_query_example_settings_page'
+        );
+}
+
+// Settings page callback function
+function secure_query_example_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1><?php esc_html_e('Secure Query Example Settings', 'secure-query-example'); ?></h1>
+        <p><?php esc_html_e('This is the settings page for the Secure Query Example plugin.', 'secure-query-example'); ?></p>
+        <!-- Add your settings or configuration options here -->
+    </div>
+    <?php
+}
+
 // Example shortcode to demonstrate a secure database query
 add_shortcode('secure_query_example', 'secure_query_example_shortcode');
 function secure_query_example_shortcode($atts, $content = '') {
     global $wpdb;
-    
+
     // Sanitize and prepare user input
     $login    = sanitize_text_field('hacker');
     $password = sanitize_text_field("123456' OR 1='1");
-    
+
     // Prepare the SQL query using $wpdb->prepare()
     $sql = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}users WHERE `login` = %s AND `pass` = %s", $login, $password);
-    
+
     // Run the query
     $results = $wpdb->get_results($sql);
-    
+
     // Process the results (this is just an example, you would handle the results based on your needs)
     $output = '<ul>';
     foreach ($results as $result) {
         $output .= '<li>' . esc_html($result->login) . '</li>';
     }
     $output .= '</ul>';
-    
+
     return $output;
 }
