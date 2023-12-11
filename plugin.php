08d2e2928d0043f48ee5359c5c66796d0777bfa2
ediemcaech <emekic@np.ac.rs>
2023-12-11 21:27:43 +0100

    Formiramo osnovnu strukturu PlugIn-a

--- plugin.php
+++ plugin.php
@@ -1,9 +1,27 @@
+
+
 <?php
 /**
- * Plugin Name: Formatting SQL Statements
+ * Plugin Name: SQL Sanitizer
  * Plugin URI: http://example.com/
- * Description: Formatting SQL Statements
+ * Description: Plugin for updating post title and retrieving post details using SQL sanitization.
  * Author: Your Name
  * Author URI: http://yourwebsite.com/
  */
 
+// Add menu option under Settings
+add_action('admin_menu', 'sql_sanitizer_menu');
+function sql_sanitizer_menu() {
+    add_options_page(
+        'SQL Sanitizer',
+        'SQL Sanitizer',
+        'manage_options',
+        'sql-sanitizer',
+        'sql_sanitizer_settings_page'
+        );
+}
+
+// Settings page callback function
+function sql_sanitizer_settings_page() {
+
+}
