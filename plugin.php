c71641d03f3cfe0f2915a6c769350acda53ef388
ediemcaech <emekic@np.ac.rs>
2023-12-11 10:32:00 +0100

    Formirali smo plugin za manipulaciju SQL bazom

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Formatting SQL Statements
+ * Plugin URI: http://example.com/
+ * Description: Formatting SQL Statements
+ * Author: Your Name
+ * Author URI: http://yourwebsite.com/
+ */
+
